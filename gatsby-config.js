cbe64cc80a14a7531283762c0ffe5020c95f5518
ndub80 <noahfwong@gmail.com>
2018-08-21 14:36:02 -0700

    added physician + patient pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'BIOTRONIK Cardiac Diagnostics ',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 }
