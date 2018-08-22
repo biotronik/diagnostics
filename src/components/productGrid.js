241ec34684e28006096b0d7aef30d1d24fd97d60
ndub80 <noahfwong@gmail.com>
2018-08-22 10:10:22 -0700

    changed P capitialization on product component

--- src/components/productGrid.js
+++ src/components/productGrid.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Products from './Products'
+import Products from './products'
 import { Container, Row, Col } from 'reactstrap'
 
 class ProductGrid extends Component {
