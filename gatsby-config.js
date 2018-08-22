f5be49629e8de3c6099c7c62eef3d6c7658227d8
ndub80 <noahfwong@gmail.com>
2018-08-22 09:10:40 -0700

    added manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'BIOTRONIK Cardiac Diagnostics ',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+  ],
 }
