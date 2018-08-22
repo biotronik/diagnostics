20c61f699d78d062a25dbeb425f01393dd995273
ndub80 <noahfwong@gmail.com>
2018-08-22 09:13:07 -0700

    updated manifest and added icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'GatsbyJS',
-        short_name: 'GatsbyJS',
+        name: 'BIOTRONIK Cardiac Diagnostics',
+        short_name: 'Diagnostics',
         start_url: '/',
-        background_color: '#f7f0eb',
+        background_color: '#ffffff',
         theme_color: '#a2466c',
         display: 'minimal-ui',
         icon: 'src/images/icon.png', // This path is relative to the root of the site.
