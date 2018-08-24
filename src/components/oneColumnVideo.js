1bbab39f8ea18ff05f78ff3d68aa2847cdf73d49
ndub80 <noahfwong@gmail.com>
2018-08-24 05:03:34 -0700

    added wistiaPlayer component

--- src/components/oneColumnVideo.js
+++ src/components/oneColumnVideo.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import styled from 'styled-components'
-import ResponsivePlayer from './wistia'
+import ResponsivePlayer from './wistiaPlayer'
 
 const Wrapper = styled.div`
   background-color: #00234c;
