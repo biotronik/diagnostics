3520b193b17faece007184a2207bcc7c3e50de93
ndub80 <noahfwong@gmail.com>
2018-08-25 16:12:55 -0700

    added graphql to hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,20 +22,16 @@ class IndexPage extends Component {
         <Wrapper>
           <Banner
             image=""
-            title="Cardiac Diagnotics"
-            subtitle="Diagnosis begins with the right device"
+            title={this.props.data.contentfulHeroSection.title}
+            subtitle={this.props.data.contentfulHeroSection.subtitle}
             description="BIOTRONIK matches technology to the human body to advance health
           and well-being with a comprehensive portfolio of cardiovascular
           solutions that are unmatched in safety and reliability."
           />
         </Wrapper>
+
         <OneColumnText
-          text="Two innovative cardiac diagnostic platforms, MoMe Kardia and
-                BioMonitor 2, are tailored to help improve the patient’s journey
-                to health. Each tool is designed to give physicians a deeper
-                understanding of the patient’s condition, and each allows
-                patients to maintain their normal daily activities with comfort
-                and minimal interference."
+          text={this.props.data.contentfulHeroSection.description.description}
         />
 
         <ProductGrid />
@@ -45,3 +41,17 @@ class IndexPage extends Component {
 }
 
 export default IndexPage
+
+export const query = graphql`
+  query Hero {
+    contentfulHeroSection {
+      id
+      title
+      subtitle
+      description {
+        id
+        description
+      }
+    }
+  }
+`
