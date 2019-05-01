f86d55bfccaeb57b4b9d08a59b7222d67cff4a88
biotronik <noah.wong@biotronik.com>
2019-05-01 08:34:32 -0700

    deleted contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,5 @@ module.exports = {
         icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `9azfumkamx1o`,
-        accessToken: `37915af3d1f1fda7690e6a3e6047a756c502c245f2fd1f9bbc41db9c488c6f99`,
-      },
-    },
   ],
 }
