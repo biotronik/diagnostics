ff14341053b6a6e969aba4ae6af29f439d0f6c06
biotronik <noah.wong@biotronik.com>
2019-09-18 16:50:10 -0700

    removed google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,20 +18,5 @@ module.exports = {
         icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-google-tagmanager`,
-      options: {
-        id: 'GTM-5CTPHGX',
-
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // Specify optional GTM environment details.
-        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING',
-        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME',
-        // dataLayerName: 'YOUR_DATA_LAYER_NAME',
-      },
-    },
   ],
 }
